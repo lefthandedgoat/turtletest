57608ab4a062a05e2c8a34308a5f6e181c858ac8
Chris Holt <chris.holt@match.com>
2015-10-03 18:20:40 -0500

    first commit with a empty site template

---
+++ Site/src/Site/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Site")>]
+[<assembly: AssemblyProductAttribute("Site")>]
+[<assembly: AssemblyDescriptionAttribute("turtletest.com")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
