1a4c622f566e9ec065778a1c5f78503189feb0bb
Chris Holt <lefthandedgoat@gmail.com>
2015-10-04 19:51:04 -0500

    suave add the top tiles

--- Site/src/Site/html/bootstrap.fs
+++ Site/src/Site/html/bootstrap.fs
@@ -16,19 +16,19 @@ open Suave.Html
 open html_common
 
 let icon type' = italic ["class", (sprintf "fa fa-%s" type')] emptyText
-let wrapper inner = divClass "cl-wrapper" inner
+let wrapper inner = divId "cl-wrapper" inner
 let sidebar inner = divClass "cl-sidebar" inner
 let toggle inner = divClass "cl-toggle" inner
 let navblock inner = divClass "cl-navblock" inner
 let menu_space inner = divClass "menu-space" inner
 let content inner = divClass "content" inner
+let mcontent inner = divClass "cl-mcont" inner
 let sidebar_logo inner = divClass "sidebar-logo" inner
 let logo inner = divClass "logo" inner
 let vnavigation inner = ulAttr ["class", "cl-vnavigation"] inner
-//            <li><a href="applications.html"><i class="fa fa-desktop"></i>
-//                <span>Applications</span>
-//                <span class="badge badge-primary pull-right">4</span>
-//            </a></li>
+let row inner = divClass "row" inner
+let m3sm6 inner = divClass "col-md-3 col-sm-6" inner
+let tile color inner = divClass (sprintf "fd-tile detail clean tile-%s" color) inner
 
 let side_link link text' count style icon' =
   li [
@@ -36,5 +36,52 @@ let side_link link text' count style icon' =
       icon icon'
       span [text text']
       spanAttr ["class", sprintf "badge badge-%s pull-right" style] [text (string count)]
+    ]
+  ]
+
+let left_sidebar =
+  sidebar [
+    toggle [ icon "bars" ]
+    navblock [
+      menu_space [
+        content [
+          sidebar_logo [
+            logo [ aHref "index2.html" [] ]
+          ]
+          vnavigation [
+            side_link "/applications" "Applications" 4 "primary" "desktop"
+            side_link "/suites" "Suites" 19 "success" "sitemap"
+            side_link "/testcases" "Test Cases" 143 "prusia" "thumbs-up"
+            side_link "/executions" "Executions" 71 "danger" "toggle-right"
+          ]
+        ]
+      ]
+    ]
+  ]
+
+let tileContainer link text' count color icon' =
+  m3sm6 [
+    tile color [
+      content [
+        h1Class "text-left" (text (string count))
+        p (text text')
+      ]
+      divClass "icon" [ icon icon' ]
+      aHrefAttr link ["class", "details"] [
+        text "Details "
+        span [ icon "arrow-circle-right pull-right" ]
+      ]
+    ]
+  ]
+
+let home_content =
+  mcontent [
+    row [
+      tileContainer "/applications" "Applications" 4 "purple" "desktop"
+      tileContainer "/suitse" "Suites" 19 "green" "sitemap"
+      tileContainer "/testcases" "Test Cases" 143 "prusia" "thumbs-up"
+      tileContainer "/executions" "Executions" 71 "red" "toggle-right"
+    ]
+    row [
     ]
   ]
