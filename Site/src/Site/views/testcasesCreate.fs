82029ecd61c02398e240e2a0527a462facca7ee1
Chris Holt <lefthandedgoat@gmail.com>
2015-11-05 22:10:59 -0600

    test case validation

--- Site/src/Site/views/testcasesCreate.fs
+++ Site/src/Site/views/testcasesCreate.fs
@@ -45,3 +45,42 @@ let html user counts applications suites =
       testcase_content applications suites
     ]
     scripts.none
+
+let error_testcase_details applications suites errors (newTestCase : forms.NewTestCase)=
+  block_flat [
+    header [ h3 "Create Test Case" ]
+    content [
+      form_horizontal [
+        errored_label_select "Application" applications newTestCase.Application errors
+        errored_label_select "Suite" suites newTestCase.Suite errors
+        errored_label_text "Name" newTestCase.Name errors
+        errored_label_text "Version" newTestCase.Version errors
+        errored_label_text "Owners" newTestCase.Owners errors
+        label_textarea "Notes" newTestCase.Notes
+        errored_label_text "Requirements" newTestCase.Requirements errors
+        errored_label_text "Steps" newTestCase.Steps errors
+        errored_label_text "Expected" newTestCase.Expected errors
+        errored_label_text "History" newTestCase.History errors
+        errored_label_text "Attachments" newTestCase.Attachments errors
+        form_group [ sm10 [ button_save ] ]
+      ]
+    ]
+  ]
+
+let error_testcase_content applications suites errors newTestCase =
+  mcontent [
+    row [
+      m12 [
+        error_testcase_details applications suites errors newTestCase
+      ]
+    ]
+  ]
+
+let error_html user counts applications suites errors newTestCase =
+  base_html
+    "create test case"
+    [
+      partial_sidebar.left_sidebar user counts
+      error_testcase_content applications suites errors newTestCase
+    ]
+    scripts.none
