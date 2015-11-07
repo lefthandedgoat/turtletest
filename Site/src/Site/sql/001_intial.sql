4b95ba6fe4509452a243589cf7b002dd18d39c8e
Chris Holt <chris.holt@match.com>
2015-11-07 11:26:16 -0600

    reorder sql script, applications depends on users

--- Site/src/Site/sql/001_intial.sql
+++ Site/src/Site/sql/001_intial.sql
@@ -10,6 +10,15 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA turtletest GRANT SELECT ON TABLES TO turtlete
 CREATE DATABASE turtletest;
 GRANT CONNECT ON DATABASE "turtletest" to turtletest;
 
+CREATE TABLE turtletest.Users(
+  user_id       SERIAL       PRIMARY KEY NOT NULL,
+  name          varchar(64)  NOT NULL UNIQUE,
+  email         varchar(256) NOT NULL,
+  password      varchar(60)  NOT NULL,
+  scheme        smallint     NOT NULL);
+
+CREATE INDEX users_name ON turtletest.Users (name);
+
 CREATE TABLE turtletest.Applications(
   application_id SERIAL       PRIMARY KEY NOT NULL,
   user_id       integer       NOT NULL REFERENCES turtletest.Users (user_id),
@@ -21,15 +30,6 @@ CREATE TABLE turtletest.Applications(
   notes         varchar(4096) NULL);
 
 CREATE INDEX applications_fk_users ON turtletest.Applications (user_id);
-
-CREATE TABLE turtletest.Users(
-  user_id       SERIAL       PRIMARY KEY NOT NULL,
-  name          varchar(64)  NOT NULL UNIQUE,
-  email         varchar(256) NOT NULL,
-  password      varchar(60)  NOT NULL,
-  scheme        smallint     NOT NULL);
-
-CREATE INDEX users_name ON turtletest.Users (name);
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA turtletest TO turtletest;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA turtletest TO turtletest;
