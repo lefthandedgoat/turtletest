e01892f304b6880fec28cc141a33e246b9b29bae
Chris Holt <chris.holt@match.com>
2015-11-08 10:02:35 -0600

    refactor to sausage factory

--- Site/src/Site/Program.fs
+++ Site/src/Site/Program.fs
@@ -1,7 +1,6 @@
 module program
 
 open System
-
 open Suave
 open Suave.Form
 open Suave.Cookie
@@ -18,58 +17,7 @@ open Suave.Html
 open html_common
 open html_bootstrap
 open types
-
-let bindToForm form handler =
-  //todo since we manually handle errors, make bad request log errors and send you too an oops page
-  bindReq (bindForm form) handler BAD_REQUEST
-
-let sessionStore setF = context (fun x ->
-  match HttpContext.state x with
-  | Some state -> setF state
-  | None -> never)
-
-let reset =
-  unsetPair Auth.SessionAuthCookie
-  >>= unsetPair StateCookie
-  >>= FOUND paths.login
-
-let getSession f =
-  statefulForSession
-  >>= context (fun x ->
-    match x |> HttpContext.state with
-    | None -> f NoSession
-    | Some state ->
-      match state.get "user_id" with
-      | Some user_id -> f (User user_id)
-      | _ -> f NoSession)
-
-let redirectWithReturnPath redirection =
-  request (fun x ->
-    let path = x.url.AbsolutePath
-    Redirection.FOUND (redirection |> paths.withParam ("returnPath", path)))
-
-let userExists userName f_success =
-  let user = data_users.tryByName userName
-  match user with
-    | None -> NOT_FOUND "Page not found"
-    | Some(user) -> f_success user
-
-let loggedOn f_success =
-  Auth.authenticate
-    Cookie.CookieLife.Session
-    false
-    (fun () -> Choice2Of2(redirectWithReturnPath paths.login))
-    (fun _ -> Choice2Of2 reset)
-    f_success
-
-let canCreateEdit f_success user =
-  loggedOn (getSession (fun session ->
-    match session with
-    | User(_) -> f_success user session
-    | _ -> UNAUTHORIZED "Not logged in"))
-
-let canView f_success user =
-  getSession (fun sess -> f_success user sess)
+open sausage_factory
 
 let register'' =
   choose [
