7bbcf394f142a3ba35097b8628aac1acb7aa6052
Chris Holt <lefthandedgoat@gmail.com>
2016-01-07 20:56:50 -0600

    more register tests

--- Site/src/tests/tests/register.fs
+++ Site/src/tests/tests/register.fs
@@ -8,8 +8,71 @@ open page_register
 let all () =
   context "register"
 
+  before (fun _ -> goto page_register.uri)
+
+  "Name required" &&& fun _ ->
+    click "Submit"
+
+    displayed "Name is required"
+
+  "Email invalid #1" &&& fun _ ->
+    click "Submit"
+
+    displayed "Email not valid"
+
+  "Email invalid #2" &&& fun _ ->
+    "Email" << "junk"
+    click "Submit"
+
+    displayed "Email not valid"
+
+  "Password invalid #1" &&& fun _ ->
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #1" &&& fun _ ->
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #2" &&& fun _ ->
+    "Password" << "1"
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #3" &&& fun _ ->
+    "Password" << "12"
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #4" &&& fun _ ->
+    "Password" << "123"
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #5" &&& fun _ ->
+    "Password" << "1234"
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #6" &&& fun _ ->
+    "Password" << "12345"
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
+  "Password invalid #3" &&& fun _ ->
+    "Password" << "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"
+    click "Submit"
+
+    displayed "Password must between 6 and 100 characters"
+
   "Can register new unique user" &&& fun _ ->
-    goto page_register.uri
     let username, email = generateUniqueUser ()
 
     "Name" << username
